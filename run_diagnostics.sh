ec33f088727b5bdf6c041feaa590be402ee0d518
Pavel Perezhogin <pavelp@derecho3.hsn.de.hpc.ucar.edu>
2026-03-16 19:27:07 -0600

    Diagnostics script

---
+++ run_diagnostics.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Usage: ./run_diagnostics.sh experiment.yaml
+
+YAML_FILE="$1"
+BASENAME=$(basename "$YAML_FILE" .yaml)
+LOG_FILE="${BASENAME}_diagnostics.log"
+NUM_WORKERS=0
+
+# Common command parts
+PYTHON_CMD="python -u -W ignore -m"
+WORKER_ARG="--number_of_workers $NUM_WORKERS"
+
+{
+  module load conda
+  conda activate env-from-npl-2024a
+
+  $PYTHON_CMD mom6_tools.stats "$YAML_FILE" -time_series $WORKER_ARG
+  #$PYTHON_CMD mom6_tools.poleward_heat_transport "$YAML_FILE" $WORKER_ARG
+  #$PYTHON_CMD mom6_tools.moc "$YAML_FILE" $WORKER_ARG
+  #$PYTHON_CMD mom6_tools.moc_sigma2 "$YAML_FILE" $WORKER_ARG
+
+} &> "$LOG_FILE"
